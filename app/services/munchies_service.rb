73013623f1e540e5766802fd92b92d3b5fee7cf8
Blaine Kennedy <blainekennedy@gmail.com>
2024-01-15 10:55:37 -0700

    fix service, all tests passing

--- app/services/munchies_service.rb
+++ app/services/munchies_service.rb
@@ -1,8 +1,33 @@
 class MunchiesService 
   def self.conn 
     Faraday.new(url: "https://api.yelp.com/v3/") do |faraday|
-      faraday.headers["Authorization"] = Rails.application.credentials.yelp_api[:key]
+      api_key = Rails.application.credentials.dig(:yelp, :api_key)
+
+      faraday.headers["Authorization"] = "Bearer #{api_key}"
     end
   end
   
+  def self.get_restaurant(type, lat, lon)
+    response = conn.get("businesses/search", { term: type, latitude: lat, longitude: lon })
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+
+    if data[:businesses].present?
+      restaurant = data[:businesses].sample
+      hash = {
+        name: restaurant[:name], 
+        address: restaurant[:location][:display_address].join(" "),
+        rating: restaurant[:rating].to_f, 
+        reviews: restaurant[:review_count]
+      }
+    else
+     
+      hash = {
+        name: "No restaurants found",
+        address: "",
+        rating: 0.0,
+        reviews: 0
+      }
+    end
+  end
 end
--- spec/services/munchies_service_spec.rb
+++ spec/services/munchies_service_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 RSpec.describe "an API call", :vcr do 
   it "will return restaurant data based on location" do
-    restaurant = MunchiesService.get_restaurant("Juniors", 22.690468, -10.498943)
+    restaurant = MunchiesService.get_restaurant("bar", 35.481918, -97.508469)
 
     expect(restaurant).to be_a Hash 
-    expect(restaurant.keys.count).to eq(3)
+    expect(restaurant.keys.count).to eq(4)
     expect(restaurant).to have_key(:name)
     expect(restaurant[:name]).to be_a String
     expect(restaurant).to have_key(:address)
